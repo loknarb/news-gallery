408717770f102a63d47f43ce8c9cc74e8158ec82
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-05-16 20:19:22 +0200

    sidebar mobile version styling changes

--- components/body/SidebarMobile.tsx
+++ components/body/SidebarMobile.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const SidebarMobile = () => {
-  return <div>SidebarMobile showing</div>;
+  return (
+    <>
+      <div
+        className='fixed w-full h-full bg-zinc-800 bg-opacity-50 z-20 cursor-pointer inset-0'
+        onClick={}></div>
+      <aside className='p-3 h-full z-30 w-60 flex-col flex fixed left-0 bg-slate-100 top-0'>
+        <div className='self-end' onClick={}>
+          Arrow here
+        </div>
+        We want to map these
+      </aside>
+    </>
+  );
 };
 
 export default SidebarMobile;
