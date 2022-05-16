291ae4e354a82ca1c8a263c918a186a61c5b97cb
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-05-16 19:43:02 +0200

    finished initial header logic

--- components/body/MainMobile.tsx
+++ components/body/MainMobile.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NewsList from './NewsList';
 
 const MainMobile = () => {
+  const [showSideBar, setShowSideBar] = useState(false);
+
   return (
     <div className='flex flex-row md:pl-52 pt-14 h-50'>
       <NewsList />
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,9 +1,5 @@
-import Image from 'next/image';
 import React from 'react';
-import icon from '../../public/favicon.png';
 import useWindowSize from '../hooks/useWindowSize';
-import MenuLogo from './MenuLogo';
-import Search from './Search';
 
 import useDebounce from '../hooks/useDebounce';
 import HeaderTablet from './HeaderTablet';
@@ -13,10 +9,8 @@ import HeaderDesktop from './HeaderDesktop';
 const Header = () => {
   const { width } = useWindowSize();
   const debouncedValue = useDebounce<number | undefined>(width, 500);
-  // TODO left side company icon
-  // TODO header about 55px
   return (
-    <header className='w-full flex z-10 flex-row top-0 fixed items-center h-14 px-4 justify-between'>
+    <header className='w-full flex z-10 flex-row top-0 fixed items-center h-14 px-4 justify-between border-b-slate-400 border-b'>
       {width! >= 992 ? <HeaderDesktop /> : ''}
       {width! > 768 && width! < 992 ? <HeaderTablet /> : ''}
       {width! <= 768 ? <HeaderMobile /> : ''}
