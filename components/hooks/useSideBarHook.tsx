c5dd478b3a90573033b4667ad0fef78277a58c30
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-05-16 19:56:27 +0200

    quick sidebar state wide functions

--- components/hooks/useSideBarHook.tsx
+++ components/hooks/useSideBarHook.tsx
@@ -1,2 +1,15 @@
-// const useSideBar = create
-export {};
+import create from 'zustand';
+const useSideBar = create<{ sideBar: boolean; hideSideBar: () => void; showSideBar: () => void }>(
+  (set) => ({
+    sideBar: false,
+    hideSideBar: () =>
+      set(() => ({
+        sideBar: false,
+      })),
+    showSideBar: () =>
+      set(() => ({
+        sideBar: true,
+      })),
+  })
+);
+export default useSideBar;
