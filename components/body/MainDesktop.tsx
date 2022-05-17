6b9449a438b020135ddc97c759a43d5962bf6b1d
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-05-17 22:03:05 +0200

    removed unecessary code

--- components/body/MainDesktop.tsx
+++ components/body/MainDesktop.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import useSideBar from '../hooks/useSideBarHook';
-import Button from '../UI/Button';
-import RightArrow from '../UI/RightArrow';
 import NewsList from './NewsList';
 import Sidebar from './Sidebar/Sidebar';
 import SideBarLogos from './Sidebar/SideBarLogos';
 
 const MainDesktop = () => {
   const { sideBar, showSideBar } = useSideBar((state) => state);
-  console.log(sideBar);
-
   return (
     <div className='flex flex-row md:pl-52 pt-14 bg-slate-100'>
       {sideBar ? (
