dd816f6c1f66887f5fbf443f8616fcad5358b959
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-05-18 12:56:08 +0200

    implemented get and upserting article into mongodb

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { GetStaticProps, NextPage } from 'next';
 import Head from 'next/head';
 import Main from '../components/body/Main';
 import Header from '../components/header/Header';
-import { MongoClient } from 'mongodb';
+import { Document, MongoClient, UpdateResult } from 'mongodb';
 import { NewsItemProps } from './types/types';
 import axios from 'axios';
 const Home: NextPage<NewsItemProps> = ({ newsItems }) => {
@@ -33,61 +33,73 @@ export const getStaticProps: GetStaticProps = async () => {
   const mongoDB = mongo.db();
   const newsArticleCollection = mongoDB.collection('newsArticles');
   const collectionAPI = [
-    'https://jsonplaceholder.typicode.com/posts/1',
-    'https://jsonplaceholder.typicode.com/posts/2',
-    'https://jsonplaceholder.typicode.com/posts/3',
-    'https://jsonplaceholder.typicode.com/posts/7',
-    'https://jsonplaceholder.typicode.com/posts/5',
-    'https://jsonplaceholder.typicode.com/posts/6',
+    `https://api.thenewsapi.com/v1/news/all?api_token=${process.env.NEWS_API}&language=en&search=javascript&categories=business,tech&limit=5`,
+    `https://api.thenewsapi.com/v1/news/all?api_token=${process.env.NEWS_API}&language=en&search=typescript&categories=business,tech&limit=5`,
+    `https://api.thenewsapi.com/v1/news/all?api_token=${process.env.NEWS_API}&language=en&search=python&categories=business,tech&limit=5`,
+    `https://api.thenewsapi.com/v1/news/all?api_token=${process.env.NEWS_API}&language=en&search=react+(typescript)&categories=business,tech&limit=5`,
+    `https://api.thenewsapi.com/v1/news/all?api_token=${process.env.NEWS_API}&language=en&search=react+(javascript)&categories=business,tech&published_on=2022-05-17&limit=5`,
   ];
   // console.log(`${process.env.MONGO_DB_API}`);
-  Promise.all(collectionAPI.map((endpoint) => axios.get(endpoint))).then(
-    async ([
-      { data: title },
-      { data: repos },
-      { data: followers },
-      { data: followings },
-      { data: test },
-      { data: tester },
-    ]) => {
-      const data = [title, repos, followers, followings, test, tester];
-      const data2 = [title, repos, followers, followings, test, tester];
-      const data3 = [title, repos, followers, followings, test, tester];
-      const data4 = [title, repos, followers, followings, test, tester];
-      const data5 = [title, repos, followers, followings, test, tester];
-      const data6 = [title, repos, followers, followings, test, tester];
-      const actualCollection = [...data, ...data2, ...data3, ...data4, ...data5, ...data6];
-      const mongo = await MongoClient.connect(`${process.env.MONGO_DB_API}`);
-      const mongoDB = mongo.db();
-      const newsArticleCollection = mongoDB.collection('newsArticles');
-      // return console.log([...title.data, ...repos, ...followers, ...followings, ...test, ...tester]);
-      // https://www.mongodb.com/docs/manual/reference/method/db.collection.update/
-      //www.mongodb.com/docs/manual/reference/method/db.collection.update/#std-label-update-upsert
-      // const result = await newsArticleCollection.insertMany(data);
-      // https://stackoverflow.com/questions/46441006/how-can-i-remove-older-records-from-a-collection-in-mongodb <- date published removal
-      let resultMany: number;
-      actualCollection.forEach(async (data, i) => {
-        // console.log(data);
-        await newsArticleCollection.updateMany(
-          { uuid: data.id },
-          { $set: { userId: data.userId, id: data.id, title: data.title, body: data.body } },
-          { upsert: true }
-        );
-      });
-      // const result = await newsArticleCollection.updateOne(
-      //   { id: title.id },
-      //   { $set: { userId: title.userId, id: title.id, title: title.title, body: title.body } },
-      //   {
-      //     upsert: true,
-      //   }
-      // );
-      // console.log(resultMany);
-    }
-  );
+  // Promise.all(collectionAPI.map((endpoint) => axios.get(endpoint))).then(
+  //   async ([
+  //     { data: js },
+  //     { data: ts },
+  //     { data: python },
+  //     { data: reactts },
+  //     { data: reactjs },
+  //   ]) => {
+  //     const actualCollection = [
+  //       ...js.data,
+  //       ...ts.data,
+  //       ...python.data,
+  //       ...reactts.data,
+  //       ...reactjs.data,
+  //     ];
+  //     const mongo = await MongoClient.connect(`${process.env.MONGO_DB_API}`);
+  //     const mongoDB = mongo.db();
+  //     const newsArticleCollection = mongoDB.collection('newsArticles');
+  //     // return console.log([...title.data, ...repos, ...followers, ...followings, ...test, ...tester]);
+  //     // https://www.mongodb.com/docs/manual/reference/method/db.collection.update/
+  //     //www.mongodb.com/docs/manual/reference/method/db.collection.update/#std-label-update-upsert
+  //     // const result = await newsArticleCollection.insertMany(data);
+  //     // https://stackoverflow.com/questions/46441006/how-can-i-remove-older-records-from-a-collection-in-mongodb <- date published removal
+  //     let resultMany: UpdateResult | Document;
+  //     actualCollection.forEach(async (article, i) => {
+  //       // console.log(article);
+  //       resultMany = await newsArticleCollection.updateMany(
+  //         { uuid: article.uuid },
+  //         {
+  //           $set: {
+  //             uuid: article.uuid,
+  //             title: article.title,
+  //             description: article.description,
+  //             snippet: article.snippet,
+  //             url: article.url,
+  //             image_url: article.image_url,
+  //             source: article.source,
+  //             categories: article.categories,
+  //             published_at: article.published_at,
+  //           },
+  //         },
+  //         { upsert: true }
+  //       );
+  //       console.log(i);
+  //       console.log(resultMany);
+  //     });
+  //     // const result = await newsArticleCollection.updateOne(
+  //     //   { id: title.id },
+  //     //   { $set: { userId: title.userId, id: title.id, title: title.title, body: title.body } },
+  //     //   {
+  //     //     upsert: true,
+  //     //   }
+  //     // );
+  //   }
+  // );
   // console.log(x);
-  const response = await newsArticleCollection.find().toArray();
+  const response = await newsArticleCollection.find({}, { projection: { _id: 0 } }).toArray();
   const articleData = JSON.stringify(response);
   const newsItems = JSON.stringify(articleData);
+  console.log(newsItems);
   mongo.close();
 
   return {
