ba56c1408ebbfcafd065037e36e99440cf5c1b11
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-05-18 12:22:24 +0200

    further api configurations

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,7 @@ export const getStaticProps: GetStaticProps = async () => {
     'https://jsonplaceholder.typicode.com/posts/1',
     'https://jsonplaceholder.typicode.com/posts/2',
     'https://jsonplaceholder.typicode.com/posts/3',
-    'https://jsonplaceholder.typicode.com/posts/4',
+    'https://jsonplaceholder.typicode.com/posts/7',
     'https://jsonplaceholder.typicode.com/posts/5',
     'https://jsonplaceholder.typicode.com/posts/6',
   ];
@@ -51,11 +51,37 @@ export const getStaticProps: GetStaticProps = async () => {
       { data: tester },
     ]) => {
       const data = [title, repos, followers, followings, test, tester];
-
+      const data2 = [title, repos, followers, followings, test, tester];
+      const data3 = [title, repos, followers, followings, test, tester];
+      const data4 = [title, repos, followers, followings, test, tester];
+      const data5 = [title, repos, followers, followings, test, tester];
+      const data6 = [title, repos, followers, followings, test, tester];
+      const actualCollection = [...data, ...data2, ...data3, ...data4, ...data5, ...data6];
+      const mongo = await MongoClient.connect(`${process.env.MONGO_DB_API}`);
+      const mongoDB = mongo.db();
+      const newsArticleCollection = mongoDB.collection('newsArticles');
       // return console.log([...title.data, ...repos, ...followers, ...followings, ...test, ...tester]);
-      console.log(`${process.env.MONGO_DB_API}`);
-      const result = await newsArticleCollection.insertMany(data);
-      console.log(result);
+      // https://www.mongodb.com/docs/manual/reference/method/db.collection.update/
+      //www.mongodb.com/docs/manual/reference/method/db.collection.update/#std-label-update-upsert
+      // const result = await newsArticleCollection.insertMany(data);
+      // https://stackoverflow.com/questions/46441006/how-can-i-remove-older-records-from-a-collection-in-mongodb <- date published removal
+      let resultMany: number;
+      actualCollection.forEach(async (data, i) => {
+        // console.log(data);
+        await newsArticleCollection.updateMany(
+          { uuid: data.id },
+          { $set: { userId: data.userId, id: data.id, title: data.title, body: data.body } },
+          { upsert: true }
+        );
+      });
+      // const result = await newsArticleCollection.updateOne(
+      //   { id: title.id },
+      //   { $set: { userId: title.userId, id: title.id, title: title.title, body: title.body } },
+      //   {
+      //     upsert: true,
+      //   }
+      // );
+      // console.log(resultMany);
     }
   );
   // console.log(x);
@@ -63,6 +89,7 @@ export const getStaticProps: GetStaticProps = async () => {
   const articleData = JSON.stringify(response);
   const newsItems = JSON.stringify(articleData);
   mongo.close();
+
   return {
     props: {
       newsItems,
