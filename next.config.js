5396d71993770441cfae34df4747fa0e966a25de
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-05-18 15:10:00 +0200

    adjusted accepted image sources

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['miro.medium.com','cdn.vox-cdn.com', 'muscleshop.tdsclick.org', 's.espncdn.com']
+    domains: ['miro.medium.com','cdn.vox-cdn.com', 'muscleshop.tdsclick.org', 's.espncdn.com', 'external-preview.redd.it']
   }
 }
 
