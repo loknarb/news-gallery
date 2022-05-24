9e57980c59c8328fe554d7b8dd80a97ad229374e
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-05-24 20:00:42 +0200

    added addition and subtraction to our db based on api request

--- pages/api/popular.ts
+++ pages/api/popular.ts
@@ -1,25 +1,32 @@
 import { MongoClient } from 'mongodb';
 import type { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
-import { NewsItemType } from '../types/types';
+import { NewsItemType, UpvotePostRequest } from '../types/types';
 
 // export default function handler(req: NextApiRequest, res: NextApiResponse<NewsItemType>) {
 const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
-    const data = req.body;
+    const data: UpvotePostRequest = req.body;
+    console.log(data);
     // TODO close client or instead keep it open for a specific session
     const client = await MongoClient.connect(`${process.env.MONGO_DB_API}`);
     const db = client.db();
     const newsArticleCollection = db.collection('newsArticles');
+    let upvoteAmount: number;
+    switch (data.action) {
+      case 'ADD':
+        upvoteAmount = 1;
+        break;
+      case 'SUBTRACT':
+        upvoteAmount = -1;
+        break;
+    }
     const article = newsArticleCollection.updateOne(
-      { uuid: data.article.uuid },
+      { uuid: data.uuid },
       {
-        $set: {
-          upvoteAmount: Math.floor(Math.random() * 90),
-          commentAmount: Math.floor(Math.random() * 30),
-          upvoted: true,
+        $inc: {
+          upvoteAmount: upvoteAmount,
         },
-      },
-      { upsert: true }
+      }
     );
 
     res.status(200).json({ article: article });
