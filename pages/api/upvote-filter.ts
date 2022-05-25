2cc55f239ff7ce7cdd515ca14359e3e8275bf07c
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-05-25 09:58:08 +0200

    init bookmark and upvote api filter results

---
+++ pages/api/upvote-filter.ts
@@ -0,0 +1,6 @@
+import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
+import { MongoClient } from 'mongodb';
+const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+  }
+};
