7932d16e99b2478a965a9d6292a97f8691a51e9e
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-05-27 17:13:11 +0200

    adjusted initial search

--- components/header/Search.tsx
+++ components/header/Search.tsx
@@ -57,7 +57,7 @@ const Search = () => {
         onChange={onInputChangeHandler}
         animate={searchShown ? 'open' : 'closed'}
         variants={variants}
-        className='border border-slate-200 rounded-md p-1 pr-8 pl-2  shadow-white active:shadow hover:shadow focus:shadow focus-visible:outline-none dark:bg-[#2B2A33] dark:caret-slate-400 dark:border-slate-500'
+        className='border border-slate-200 rounded-md p-1 pr-8 pl-2 w-24 shadow-white active:shadow hover:shadow focus:shadow focus-visible:outline-none dark:bg-[#2B2A33] dark:caret-slate-400 dark:border-slate-500'
         placeholder='Article'
       />
       <Button className='-ml-8 dark:text-[#AAB6C1]'>
