45b58214c3f71afec58ef9dbc80c2fd2d0f8af65
Sebastien Brumbaugh <66973931+loknarb@users.noreply.github.com>
2022-05-27 19:51:55 +0200

    changed in order to deploy with ts compiled

--- next.config.js
+++ next.config.js
@@ -2,8 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["logo.clearbit.com", "www.joshwcomeau.com", "www.freecodecamp.org", "miro.medium.com", "blog.logrocket.com", "cdn.tutsplus.com", "cms-assets.tutsplus.com", "slack.engineering", "blog.jetbrains.com"]
-  }
-}
+    domains: [
+      'logo.clearbit.com',
+      'www.joshwcomeau.com',
+      'www.freecodecamp.org',
+      'miro.medium.com',
+      'blog.logrocket.com',
+      'cdn.tutsplus.com',
+      'cms-assets.tutsplus.com',
+      'slack.engineering',
+      'blog.jetbrains.com',
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
